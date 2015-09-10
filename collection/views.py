99774b252d33130c72fbf1fcbd2088f91456f0ee
Patrick Graham <pgraham@maildrop.cc>
2015-09-10 22:40:38 +0000

    Displaying dynamic info

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
-	number = 6
+	things = Thing.objects.all()
 	return render(request, 'index.html', {
-		'number': number,
+		'things': things,
 		})
