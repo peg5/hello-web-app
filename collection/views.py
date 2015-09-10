1d86cdf09911d5ea04bd0ab044daa7e6393c78a2
Patrick Graham <pgraham@maildrop.cc>
2015-09-10 17:00:54 +0000

    Added variable to index page and views.

--- collection/views.py
+++ collection/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	number = 6
+	return render(request, 'index.html', {
+		'number': number,
+		})
