ebcd7fc2253f08621ec814dd6d53aba94ca7d5da
Patrick Graham <pgraham@maildrop.cc>
2015-09-10 23:32:54 +0000

    Set up individual object pages

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,18 +1,3 @@
-"""hellowebapp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -25,5 +10,8 @@ urlpatterns = [
     url(r'^contact/$',
     	TemplateView.as_view(template_name='contact.html'),
     	name='contact'),
+    url(r'^things/(?P<slug>[-\w]+)/$',
+    	'collection.views.thing_detail',
+    	name='thing_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
